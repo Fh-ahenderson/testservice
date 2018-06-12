c022bb19403ee1185af14b15ab8f35126638891e
Fh-ahenderson <ahenderson@fischerhomes.com>
2018-06-12 09:39:08 -0400

    Add files via upload

---
+++ TestService/TestService/DIContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace TestService
+{
+    public class DIContext : DbContext
+    {
+
+        public DIContext() :base("DIContext")
+        {
+
+        }
+
+        public DbSet<DIObject> DIObjects { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+                modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
